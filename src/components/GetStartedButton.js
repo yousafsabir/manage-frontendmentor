dee382df72b8b1cb8d5c9f621332b16f463530d9
yousafsabir <yousafsabir430516@gmail.com>
2022-06-10 19:05:47 +0500

    getStarted button separated

---
+++ src/components/GetStartedButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const GetStartedButton = () => {
+    return (
+        <a
+            href="/"
+            className=" self-baseline rounded-full bg-brightRed p-3 px-6 pt-2 text-white hover:bg-brightRedLight"
+        >
+            Get Started
+        </a>
+    );
+};
+
+export default GetStartedButton;
